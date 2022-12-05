06ef710fc27caf4791203b14c1d1a99dff39b938
Pritam Das <pritamd47@gmail.com>
2022-12-04 23:53:23 -0800

    projected passed or not

--- src/reservoirnetwork/network.py
+++ src/reservoirnetwork/network.py
@@ -1,6 +1,8 @@
 from landlab import NetworkModelGrid
 
 class ReservoirNetwork(NetworkModelGrid):
-    def __init__(self, yx_of_node, links, xy_axis_name=..., xy_axis_units="-", xy_of_reference=...):
-        super().__init__(yx_of_node, links, xy_axis_name, xy_axis_units, xy_of_reference)
-    
+    def __init__(self, yx_of_node, links, xy_of_reference=None, xy_axis_name=('lon', 'lat'), xy_axis_units=('deg', 'deg')):
+        if xy_of_reference is not None:
+            super().__init__(yx_of_node, links, xy_axis_name, xy_axis_units, xy_of_reference)
+        else:
+            super().__init__(yx_of_node, links, xy_axis_name, xy_axis_units, xy_of_reference=(yx_of_node[1], yx_of_node[0]))
